472d18249fc98797cb94117c8dbea786b5fb7208
Dániel Buga <bugadani@gmail.com>
2019-05-06 20:08:51 +0200

    Implement a new communication manager that supports pending response and multiple validation methods
    This will be the basis of a new communication protocol that does not rely on a special long message command, but supports both long and fast messages.

---
+++ rrrc/communication/CommunicationManager.c
@@ -0,0 +1,153 @@
+/*
+ * CommunicationManager.c
+ *
+ * Created: 2019. 05. 06. 18:02:54
+ *  Author: Dániel Buga
+ */ 
+
+#include "CommunicationManager.h"
+#include "utils_assert.h"
+
+#include "utils/crc.h"
+#include <stdbool.h>
+
+static const Comm_CommandHandler_t* comm_commandTable = NULL;
+static size_t comm_commandTableSize = 0u;
+
+/* private helpers */
+static bool _checkChecksum(const Comm_Command_t* command)
+{
+    /* calculate CRC, skipping the CRC field */
+    uint16_t headerCrc = CRC16_Calculate(0xFFFFu, (const uint8_t*) command, sizeof(Comm_Operation_t) + sizeof(uint8_t) + sizeof(uint8_t));
+    uint16_t messageCrc = CRC16_Calculate(headerCrc, (const uint8_t*) command->payload, command->payloadLength);
+
+    return command->checksum == messageCrc;
+}
+
+void Comm_Init(const Comm_CommandHandler_t* commandTable, size_t commandTableSize)
+{
+    ASSERT(commandTable);
+
+    if (comm_commandTable != NULL)
+    {
+        for (size_t i = 0u; i < comm_commandTableSize; i++)
+        {
+            comm_commandTable[i].Cancel();
+        }
+    }
+
+    comm_commandTable     = commandTable;
+    comm_commandTableSize = commandTableSize;
+}
+
+static Comm_Status_t _handleOperation_Cancel(const Comm_Command_t* command)
+{
+    if (comm_commandTable[command->command].Cancel != NULL)
+    {
+        comm_commandTable[command->command].Cancel();
+    }
+
+    return Comm_Status_Ok;
+}
+
+static Comm_Status_t _handleOperation_GetResult(const Comm_Command_t* command, uint8_t* responseBuffer, uint8_t payloadBufferSize, uint8_t* responseLength)
+{
+    if (comm_commandTable[command->command].GetResult == NULL)
+    {
+        return Comm_Status_Error_InternalError;
+    }
+    else
+    {
+        return comm_commandTable[command->command].GetResult(responseBuffer, payloadBufferSize, responseLength);
+    }
+}
+
+static Comm_Status_t _handleOperation_Start(const Comm_Command_t* command, uint8_t* responseBuffer, uint8_t payloadBufferSize, uint8_t* responseLength)
+{
+    Comm_Status_t resultStatus = comm_commandTable[command->command].Start((const uint8_t*) command->payload, command->payloadLength, responseBuffer, payloadBufferSize, responseLength);
+    if (resultStatus == Comm_Status_Pending)
+    {
+        return _handleOperation_GetResult(command, responseBuffer, payloadBufferSize, responseLength);
+    }
+    else
+    {
+        return resultStatus;
+    }
+}
+
+/**
+ * Handle a request and prepare a response
+ *
+ * @return the response length in bytes
+ */
+size_t Comm_Handle(const Comm_Command_t* command, Comm_Response_t* response, size_t responseBufferSize)
+{
+    const size_t responseHeaderSize = sizeof(Comm_Status_t) + sizeof(uint8_t) + sizeof(uint16_t);
+    
+    ASSERT(responseBufferSize > responseHeaderSize);
+    ASSERT((responseBufferSize - responseHeaderSize) < 256u); /* protocol limitation */
+    uint8_t payloadBufferSize = responseBufferSize - responseHeaderSize;
+    uint8_t payloadSize = 0u;
+    Comm_Status_t resultStatus;
+
+    if (!_checkChecksum(command))
+    {
+        /* integrity error */
+        resultStatus = Comm_Status_Error_IntegrityError;
+    }
+    else if (command->command >= comm_commandTableSize || comm_commandTable[command->command].Start == NULL)
+    {
+        /* unimplemented command */
+        resultStatus = Comm_Status_Error_UnknownCommand;
+    }
+    else
+    {
+        switch (command->operation)
+        {
+            case Comm_Operation_Cancel:
+                resultStatus = _handleOperation_Cancel();
+                break;
+
+            case Comm_Operation_Start:
+                resultStatus = _handleOperation_Start(command, (uint8_t*) response->payload, payloadBufferSize, &payloadSize);
+                break;
+
+            case Comm_Operation_Restart:
+                (void) _handleOperation_Cancel();
+                resultStatus = _handleOperation_Start(command, (uint8_t*) response->payload, payloadBufferSize, &payloadSize);
+                break;
+
+            case Comm_Operation_GetResult:
+                resultStatus = _handleOperation_GetResult(command, (uint8_t*) response->payload, payloadBufferSize, &payloadSize);
+                break;
+
+            default:
+                resultStatus = Comm_Status_Error_UnknownOperation;
+                break;
+        }
+    }
+
+    /* detect overwrite */
+    if (payloadSize > payloadBufferSize)
+    {
+        resultStatus = Comm_Status_Error_InternalError;
+    }
+
+    /* only certain responses may contain a payload */
+    if (resultStatus != Comm_Status_Ok && resultStatus != Comm_Status_Error_CommandError)
+    {
+        payloadSize = 0u;
+    }
+
+    /* fill header */
+    response->status = resultStatus;
+    response->payloadLength = payloadSize;
+    
+    /* calculate CRC, skipping the CRC field */
+    uint16_t headerCrc = CRC16_Calculate(0xFFFFu, (const uint8_t*) response, sizeof(Comm_Status_t) + sizeof(uint8_t));
+    uint16_t messageCrc = CRC16_Calculate(headerCrc, (const uint8_t*) response->payload, response->payloadLength);
+
+    response->checksum = messageCrc;
+
+    return payloadSize + responseHeaderSize;
+}
---
+++ rrrc/communication/CommunicationManager.h
@@ -0,0 +1,88 @@
+/*
+ * CommunicationManager.h
+ *
+ * This file defines the implemented communication interface above the I2C protocol.
+ *
+ * Created: 2019. 05. 06. 17:40:11
+ *  Author: Dániel Buga
+ */ 
+
+#ifndef COMMUNICATION_MANAGER_H_
+#define COMMUNICATION_MANAGER_H_
+
+#include <stdio.h>
+
+typedef enum 
+{
+    Comm_Status_Ok,      /* operation executed successfully, response may contain payload */
+    Comm_Status_Busy,    /* a command is currently pending, can't start a new one */
+    Comm_Status_Pending, /* command execution is in progress, no data supplied */
+    
+    Comm_Status_Error_UnknownOperation,
+    Comm_Status_Error_InvalidOperation, /* in case GetResult is used but no command is pending */
+    Comm_Status_Error_IntegrityError,   /* checksum field and calculated checksum do not match */
+    Comm_Status_Error_PayloadLengthError, /* data lost because message was too long */
+    Comm_Status_Error_UnknownCommand,
+    Comm_Status_Error_CommandError, /* response contains additional command-specific error data */
+    Comm_Status_Error_InternalError /* other errors indicating internal programming errors */
+} Comm_Status_t;
+
+typedef enum
+{
+    Comm_Operation_Start,
+    Comm_Operation_Restart,
+    Comm_Operation_GetResult,
+    Comm_Operation_Cancel
+} Comm_Operation_t;
+
+typedef struct 
+{
+    Comm_Operation_t operation;
+    uint8_t command;
+    uint8_t payloadLength;
+    uint16_t checksum;
+    uint8_t payload[];
+}
+__attribute__((packed)) Comm_Command_t;
+
+typedef struct
+{
+    Comm_Status_t status;
+    uint8_t payloadLength;
+    uint16_t checksum;
+    uint8_t payload[];
+}
+__attribute__((packed)) Comm_Response_t;
+
+/**
+ * Callback type that defines a command handler
+ *
+ * Command handlers are called by Comm_Handle and run in the communication thread context, in parallel with other application logic
+ */
+ typedef Comm_Status_t (*Comm_CommandHandler_Start_t)(const uint8_t* commandPayload, uint8_t commandSize, uint8_t* response, uint8_t responseBufferSize, uint8_t* responseCount);
+ typedef Comm_Status_t (*Comm_CommandHandler_GetResult_t)(uint8_t* response, uint8_t responseBufferSize, uint8_t* responseCount);
+ typedef void (*Comm_CommandHandler_Cancel_t)(void);
+
+ typedef struct 
+ {
+     Comm_CommandHandler_Start_t Start;         /* may be NULL in case of an unimplemented command */
+     Comm_CommandHandler_GetResult_t GetResult; /* may be NULL when command processing is short */
+     Comm_CommandHandler_Cancel_t Cancel;       /* may be NULL */
+ } Comm_CommandHandler_t;
+
+/**
+ * Initialize the communication handler
+ *
+ * Sets up command handler to accept and handle commands by calling their respective handlers.
+ * Command handlers are defined in the commandTable variable.
+ */
+void Comm_Init(const Comm_CommandHandler_t* commandTable, size_t commandTableSize);
+
+/**
+ * Handle a request and prepare a response
+ *
+ * @return the response length in bytes
+ */
+size_t Comm_Handle(const Comm_Command_t* command, Comm_Response_t* response, size_t responseBufferSize);
+
+#endif /* COMMUNICATION_MANAGER_H_ */
