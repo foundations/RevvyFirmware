e42d3e62d609164fdab939237d6ed5aa71282fc8
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-05-10 22:06:34 +0200

    Move FreeRTOS into third_party folder

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -16,8 +16,6 @@ void assert_triggered(const char *file, uint32_t line);
 /* Higher number means higher priority */
 #define taskPriority_Main              1
 #define taskPriority_Communication     2
-#define taskPriority_UltrasoundHandler 1
-#define taskPriority_UltrasoundChecker 1
 
 // <h> Basic
 
