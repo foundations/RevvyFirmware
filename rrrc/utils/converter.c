8cfe7ead84534d1a7083746954b26d3b049a8e48
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-05-25 21:00:40 +0200

    Read data using little endian byte order

--- rrrc/utils/converter.c
+++ rrrc/utils/converter.c
@@ -9,14 +9,10 @@
 
 int32_t get_int32(const uint8_t* buffer)
 {
-    uint32_t bytes = buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3] << 0;
-
-    return (int32_t) (bytes);
+    return *(int32_t*) buffer;
 }
 
 float get_float(const uint8_t* buffer)
 {
-    uint32_t bytes = buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3] << 0;
-
-    return *(float*) &bytes;
+    return *(float*) buffer;
 }
