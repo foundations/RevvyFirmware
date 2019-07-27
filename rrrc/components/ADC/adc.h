aad780af99480d5ae618755f471bfdc94781dcec
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-07-27 11:20:53 +0200

    Add missing function declaration

--- rrrc/components/ADC/adc.h
+++ rrrc/components/ADC/adc.h
@@ -22,6 +22,8 @@ void ADC_Run_Update(void);
 void ADC_Write_RawSamples_ADC0(uint16_t smaples[4]);
 void ADC_Write_RawSamples_ADC1(uint16_t smaples[5]);
 
+void ADC_Write_ChannelData(uint32_t adc_idx, uint32_t channel_idx, uint16_t adc_data);
+
 void ADC_Write_Samples_ADC0(float smaples[4]);
 void ADC_Write_Samples_ADC1(float smaples[5]);
 
