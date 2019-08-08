2a576d12408f4e1733e7749b9c74487288bfb031
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-08-08 15:17:26 +0200

    Update framework version before committing

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "334"
-#define FW_VERSION_NUMBER 334
+#define FW_VERSION "335"
+#define FW_VERSION_NUMBER 335
 #endif
