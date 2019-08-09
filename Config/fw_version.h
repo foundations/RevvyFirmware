6d41a301f72828bfde1d0131e3d4ed70ec343367
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-08-09 21:09:22 +0200

    Wait for flash programming to end

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "335"
-#define FW_VERSION_NUMBER 335
+#define FW_VERSION "336"
+#define FW_VERSION_NUMBER 336
 #endif
