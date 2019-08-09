c895cd5f5daaf320bf5e119360f802c4774cb048
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-08-09 09:24:25 +0200

    Clear data field properly

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "380"
-#define FW_VERSION_NUMBER 380
+#define FW_VERSION "381"
+#define FW_VERSION_NUMBER 381
 #endif
