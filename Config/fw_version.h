c94670bff0b0fb047d22b2c129d2be4c358e899b
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-08-24 22:34:36 +0800

    Remove sram linker descriptor, fix cproject include paths

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "414"
-#define FW_VERSION_NUMBER 414
+#define FW_VERSION "415"
+#define FW_VERSION_NUMBER 415
 #endif
