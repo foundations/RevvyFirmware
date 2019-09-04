010c0285879ee0f8d13cc4366c657513619fcb7f
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-09-04 20:57:43 +0200

    Require pystache to generate source code skeletons in the future

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "420"
-#define FW_VERSION_NUMBER 420
+#define FW_VERSION "421"
+#define FW_VERSION_NUMBER 421
 #endif
