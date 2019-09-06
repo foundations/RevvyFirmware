ff71191cd0fc5f8ac135800bb7eeeda9a38f4b49
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-09-06 15:38:34 +0200

    Use pyhton3 on travis

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "431"
-#define FW_VERSION_NUMBER 431
+#define FW_VERSION "433"
+#define FW_VERSION_NUMBER 433
 #endif
