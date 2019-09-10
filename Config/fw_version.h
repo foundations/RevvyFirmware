6baab532ce226912d176881a54b56cd06a782d91
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-09-10 17:16:03 +0200

    Remove interrupts from high resolution timer
    Prevents freezing

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "443"
-#define FW_VERSION_NUMBER 443
+#define FW_VERSION "444"
+#define FW_VERSION_NUMBER 444
 #endif
