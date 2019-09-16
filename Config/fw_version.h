8d540b5af5cade11f1f33df50b4e25d369cdf386
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-09-16 14:06:48 +0200

    React faster when ultrasonic sensor distance gets smaller

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "555"
-#define FW_VERSION_NUMBER 555
+#define FW_VERSION "556"
+#define FW_VERSION_NUMBER 556
 #endif
