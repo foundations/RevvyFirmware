7ff976492e3127764924988614fbf603f3f37769
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-09-20 19:20:20 +0200

    Ignore numbered backups as well

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "593"
-#define FW_VERSION_NUMBER 593
+#define FW_VERSION "594"
+#define FW_VERSION_NUMBER 594
 #endif
