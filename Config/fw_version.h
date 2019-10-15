baf34ac2794d452a9d91715f996abc98d280114c
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-10-15 10:50:20 +0200

    Bump version

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "611"
-#define FW_VERSION_NUMBER 611
+#define FW_VERSION "667"
+#define FW_VERSION_NUMBER 667
 #endif
