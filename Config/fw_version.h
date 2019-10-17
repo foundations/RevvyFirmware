14544f3840a4decc47574fb3f95abe489c537064
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-10-17 15:18:38 +0200

    Only commit tagged firmware to framework repo

--- Config/fw_version.h
+++ Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "729"
-#define FW_VERSION_NUMBER 729
+#define FW_VERSION "730"
+#define FW_VERSION_NUMBER 730
 #endif
